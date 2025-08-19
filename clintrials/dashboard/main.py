d5b14e8d352d6cfa063be9c299ffe6d243f3c258
Frederick de Ruiter <127706008+fderuiter@users.noreply.github.com>
2025-08-19 11:06:09 -0500

    Add win ratio simulation to dashboard

--- clintrials/dashboard/main.py
+++ clintrials/dashboard/main.py
@@ -2,7 +2,7 @@ import json
 
 import streamlit as st
 
-from clintrials.dashboard.views import crm_view, efftox_view
+from clintrials.dashboard.views import crm_view, efftox_view, winratio_view
 
 
 def main():
@@ -11,23 +11,26 @@ def main():
     st.sidebar.header("Select Trial Design")
     design_type = st.sidebar.selectbox(
         "Choose the type of trial design for your simulation results:",
-        ("CRM", "EffTox"),
+        ("CRM", "EffTox", "Win Ratio"),
     )
 
-    st.sidebar.header("Upload Simulation Results")
-    uploaded_file = st.sidebar.file_uploader(
-        "Upload a JSON file with simulation results", type=["json"]
-    )
+    if design_type == "Win Ratio":
+        winratio_view.render()
+    else:
+        st.sidebar.header("Upload Simulation Results")
+        uploaded_file = st.sidebar.file_uploader(
+            "Upload a JSON file with simulation results", type=["json"]
+        )
 
-    if uploaded_file is not None:
-        string_data = uploaded_file.getvalue().decode("utf-8")
-        sims = json.loads(string_data)
-        st.sidebar.success(f"Successfully loaded {len(sims)} simulations.")
+        if uploaded_file is not None:
+            string_data = uploaded_file.getvalue().decode("utf-8")
+            sims = json.loads(string_data)
+            st.sidebar.success(f"Successfully loaded {len(sims)} simulations.")
 
-        if design_type == "CRM":
-            crm_view.render(sims)
-        elif design_type == "EffTox":
-            efftox_view.render(sims)
+            if design_type == "CRM":
+                crm_view.render(sims)
+            elif design_type == "EffTox":
+                efftox_view.render(sims)
 
 
 if __name__ == "__main__":
